9f46df8c5e2404aba183bd2e1d3460e1c9d4535b
shashanksp <Shashankprince523@gmail.com>
2024-05-29 19:22:15 +0530

    fixed the crash

--- app/src/main/java/com/example/dependencyinjectionwithkoin/MainActivity.kt
+++ app/src/main/java/com/example/dependencyinjectionwithkoin/MainActivity.kt
@@ -14,17 +14,18 @@ import org.koin.core.scope.Scope
 
 class MainActivity : ComponentActivity(), AndroidScopeComponent {
     // can be used like this in xml project
-   // private val viewModel by viewModel<MainViewModel>()
+   // private val viewModel y viewModel<MainViewModel>()
 
     //any object needed immediate injection
-    private val api = get<myApi>()
+    //private val api = get<myApi>()
 
     //for lazy injection of objects
-    private val myApi by inject<myApi>()
+   // private val myApi by inject<myApi>()
 
-    //scope
+    //scoped injection
     override val scope:Scope by activityScope()
     private val hello by inject<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         println(hello)
